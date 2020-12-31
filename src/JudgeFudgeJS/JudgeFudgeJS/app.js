db8352d8d55dac03ecbafc4b4c11ff4ccdeb80a3
AllenMicrotech <53824310+AllenMicrotech@users.noreply.github.com>
2020-12-30 21:43:25 -0700

    added ignore and discord.js

--- src/JudgeFudgeJS/JudgeFudgeJS/app.js
+++ src/JudgeFudgeJS/JudgeFudgeJS/app.js
@@ -1,3 +1,21 @@
-'use strict';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-console.log('Hello world');
+var token = ''; //CLEAR THIS BEFORE SHARING CODE!!
+
+client.on('ready', () => {
+    client.user.setActivity("Under Development"); 
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    /*
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+    */
+
+});
+
+
+client.login(token);
